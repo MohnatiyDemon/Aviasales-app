1754a5114d3fd117003b3fa21fbb054b3d218568
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-09-27 15:33:52 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Logo from './components/Logo/Logo'
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <div>Hello World!</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactComponent as AviaSalesLogo } from '../../assets/Logo.svg'
+
+const Logo = () => {
+  return (
+    <>
+      <AviaSalesLogo />
+    </>
+  )
+}
+
+export default Logo
