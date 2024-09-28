0daa0399541dde5e3ae5c422d390345e8a1adcef
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-09-28 16:05:00 +0300

    maybe

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+      },
+    },
+  }
 })
