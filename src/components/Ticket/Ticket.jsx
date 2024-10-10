f1bb5c5187f51434e9de57ff2684141aa08bf3b6
Tamerlan <118521890+MohnatiyDemon@users.noreply.github.com>
2024-10-10 18:26:43 +0300

    fix styles

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -24,9 +24,11 @@ const Ticket = ({ data }) => {
         <span className={styles.Ticket__price}>{price.toLocaleString('ru-RU')} Р</span>
         <img className={styles.Ticket__logo} src={logoUrl} alt={`Airline Logo: ${carrier}`} />
       </div>
+
       <div className={styles.Ticket__info}>
-        <div className={styles.Ticket__segment}>
-          <div className={styles.Ticket__route}>
+        <div className={styles.Ticket__row}>
+          <div className={styles.Ticket__column}>
+            {' '}
             <div className={styles.Ticket__cities}>
               {outboundSegment.origin} – {outboundSegment.destination}
             </div>
@@ -35,18 +37,7 @@ const Ticket = ({ data }) => {
               {formatTime(new Date(new Date(outboundSegment.date).getTime() + outboundSegment.duration * 60000))}
             </div>
           </div>
-          <div className={styles.Ticket__length}>
-            <div className={styles.Ticket__way}>В пути</div>
-            <div className={styles.Ticket__timeLength}>{formatDuration(outboundSegment.duration)}</div>
-          </div>
-          <div className={styles.Ticket__stops}>
-            <div className={styles['Ticket__stops-count']}>{stopsCountText(outboundSegment.stops)}</div>
-            <div className={styles['Ticket__stops-cities']}>{stopsInfo(outboundSegment.stops)}</div>
-          </div>
-        </div>
-
-        <div className={styles.Ticket__segment}>
-          <div className={styles.Ticket__route}>
+          <div className={styles.Ticket__column}>
             <div className={styles.Ticket__cities}>
               {returnSegment.origin} – {returnSegment.destination}
             </div>
@@ -55,11 +46,26 @@ const Ticket = ({ data }) => {
               {formatTime(new Date(new Date(returnSegment.date).getTime() + returnSegment.duration * 60000))}
             </div>
           </div>
-          <div className={styles.Ticket__length}>
+        </div>
+
+        <div className={styles.Ticket__row}>
+          <div className={styles.Ticket__column}>
+            <div className={styles.Ticket__way}>В пути</div>
+            <div className={styles.Ticket__timeLength}>{formatDuration(outboundSegment.duration)}</div>
+          </div>
+
+          <div className={styles.Ticket__column}>
             <div className={styles.Ticket__way}>В пути</div>
             <div className={styles.Ticket__timeLength}>{formatDuration(returnSegment.duration)}</div>
           </div>
-          <div className={styles.Ticket__stops}>
+        </div>
+
+        <div className={styles.Ticket__row}>
+          <div className={styles.Ticket__column}>
+            <div className={styles['Ticket__stops-count']}>{stopsCountText(outboundSegment.stops)}</div>
+            <div className={styles['Ticket__stops-cities']}>{stopsInfo(outboundSegment.stops)}</div>
+          </div>
+          <div className={styles.Ticket__column}>
             <div className={styles['Ticket__stops-count']}>{stopsCountText(returnSegment.stops)}</div>
             <div className={styles['Ticket__stops-cities']}>{stopsInfo(returnSegment.stops)}</div>
           </div>
